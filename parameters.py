284593b473bb3408028a76845c7b6552625a5e55
Robin JEAN <robin.jean7@gmail.com>
2025-11-24 11:11:53 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -7,16 +7,16 @@ input = f"{path}/input/data/{data}.lp" #your data lp file path
 ## Conflict minimization
 
 conf_type = "non_binary" #choose either "binary" or "non_binary"
-conf_queries = f"{path}/input/conflict_queries/conflictQueries_binary.lp" #your conflict queries lp file path
+conf_queries = f"{path}/input/conflict_queries/conflictQueries_non_binary.lp" #your conflict queries lp file path
 
-conflicts = f"{path}/output/conflicts/{data}_conf_minPython.lp" #your conflicts lp file path
+conflicts = f"{path}/output/conflicts/{data}_conf_minPython.lp" #path of the file where the computed conflicts will be written
 log_conf =  f"{path}/output/log/log_conf_{data}.txt"  #your log file path for the conflict minimization
 
 
 ##Potential answers computation
 query = "q5"
 query_path = f"{path}/input/queries/{query}.lp"
-query_potAns = f"{path}/input/potential_answers/{data}/{query}" #your query potantial answers folder path
+query_potAns = f"{path}/input/potential_answers/{data}/{query}" #path of the folder where the files corresponding to the computed potential answers will be created
 log_potAns = f"{path}/output/log/log_potAns_{data}_{query}.lp"
 
 ##Preference computation
@@ -24,7 +24,7 @@ log_potAns = f"{path}/output/log/log_potAns_{data}_{query}.lp"
 method = "going_up"  #choose from "going_up", "going_down" or "grounded_extension" 
 preference_rules = f"{path}/input/preference_rules/preference_rules_a.lp" #your preference rules lp file path (here scenario a)
 
-priority_relation = f"{path}/output/priority_relations/{data}_pref_a_{method}.lp" #your output file path
+priority_relation = f"{path}/output/priority_relations/{data}_pref_a_{method}.lp" #path of the file where the computed priority relation will be written
 log_pref =  f"{path}/output/log/log_pref_a_{data}_{method}.txt"  #your log file path for the preference computation
 
 
